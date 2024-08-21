564fbac5fa4d836797fcfdc0d7385d0982580c92
Ilya Piminov <ilyapiminov@gmail.com>
2024-08-21 18:56:20 +0500

    редактирование файла test_items

--- test_items.py
+++ test_items.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_cart_button(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    time.sleep(30)
+    add_to_basket = browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket").text
+    assert add_to_basket in ["Добавить в корзину", "Add to basket", "Añadir al carrito", "Ajouter au panier"], f"Текст у кнопки должен быть: {add_to_basket}"
+
