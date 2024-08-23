11b9e551ebf3df3e9d92ca60f21d360b1a569ced
Ilya Piminov <ilyapiminov@gmail.com>
2024-08-23 14:00:59 +0500

    правка test_items

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_cart_button(browser):
     browser.get(link)
-    time.sleep(30)
-    add_to_basket = browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket").text
-    assert add_to_basket in ["Добавить в корзину", "Add to basket", "Añadir al carrito", "Ajouter au panier"], f"Текст у кнопки должен быть: {add_to_basket}"
+    time.sleep(10)
+    add_to_basket = browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert add_to_basket.is_displayed(), f"Кнопка {add_to_basket} отсутствует"
 
